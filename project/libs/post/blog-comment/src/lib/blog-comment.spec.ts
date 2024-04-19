ea63e3eab58390ea46e9f7c95bf24841600a650d
IvanMonichev <monichevid@baltech.ru>
2024-04-19 09:45:13 +0300

    feat: generate new module for comments

---
+++ project/libs/post/blog-comment/src/lib/blog-comment.spec.ts
@@ -0,0 +1,7 @@
+import { blogComment } from './blog-comment'
+
+describe('blogComment', () => {
+  it('should work', () => {
+    expect(blogComment()).toEqual('blog-comment')
+  })
+})
---
+++ project/libs/post/blog-comment/src/lib/blog-comment.ts
@@ -0,0 +1,3 @@
+export function blogComment(): string {
+  return 'blog-comment'
+}
