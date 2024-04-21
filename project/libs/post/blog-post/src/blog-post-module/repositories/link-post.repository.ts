a6078c66e4e3482e4044c341d2e8bd821fba99cc
IvanMonichev <risen15@yandex.ru>
2024-04-21 13:53:44 +0300

    feat: implement other types of posts

--- project/libs/post/blog-post/src/blog-post-module/repositories/link-post.repository.ts
+++ project/libs/post/blog-post/src/blog-post-module/repositories/link-post.repository.ts
@@ -1,14 +1,36 @@
 import { Injectable } from '@nestjs/common'
-import { BaseMemoryRepository } from '@project/data-access'
+import { BaseMemoryRepository, BasePostgresRepository } from '@project/data-access'
+import { PrismaClientService } from '@project/post-models'
 import { PostType } from '@project/shared/core'
 import { RepositoryType } from 'libs/post/blog-post/src/blog-post-module/decorators/repository-type.decorator'
 import { LinkPostEntity } from 'libs/post/blog-post/src/blog-post-module/entities/link-post.entity'
+import { VideoPostEntity } from 'libs/post/blog-post/src/blog-post-module/entities/video-post.entity'
 import { LinkPostFactory } from 'libs/post/blog-post/src/blog-post-module/factories/link-post.factory'
 
 @Injectable()
 @RepositoryType(PostType.Link)
-export class LinkPostRepository extends BaseMemoryRepository<LinkPostEntity> {
-  constructor(protected readonly entityFactory: LinkPostFactory) {
-    super(entityFactory)
+export class LinkPostRepository extends BasePostgresRepository<LinkPostEntity> {
+  constructor(protected readonly entityFactory: LinkPostFactory, readonly client: PrismaClientService) {
+    super(entityFactory, client)
+  }
+
+  public async save(entity: VideoPostEntity): Promise<void> {
+    const pojoEntity = entity.toPOJO()
+    const record = await this.client.post.create({
+      data: {
+        ...pojoEntity,
+        tags: {
+          connect: pojoEntity.tags.map(({ id }) => ({ id }))
+        },
+        comments: { connect: [] },
+        postLink: { create: pojoEntity.postLink },
+        postPhoto: undefined,
+        postQuote: undefined,
+        postText: undefined,
+        postVideo: undefined
+      }
+    })
+
+    entity.id = record.id
   }
 }
--- project/libs/post/blog-post/src/blog-post-module/repositories/photo-post.repository.ts
+++ project/libs/post/blog-post/src/blog-post-module/repositories/photo-post.repository.ts
@@ -1,14 +1,36 @@
 import { Injectable } from '@nestjs/common'
-import { BaseMemoryRepository } from '@project/data-access'
+import { BasePostgresRepository } from '@project/data-access'
+import { PrismaClientService } from '@project/post-models'
 import { PostType } from '@project/shared/core'
 import { RepositoryType } from 'libs/post/blog-post/src/blog-post-module/decorators/repository-type.decorator'
 import { PhotoPostEntity } from 'libs/post/blog-post/src/blog-post-module/entities/photo-post.entity'
+import { VideoPostEntity } from 'libs/post/blog-post/src/blog-post-module/entities/video-post.entity'
 import { PhotoPostFactory } from 'libs/post/blog-post/src/blog-post-module/factories/photo-post.factory'
 
 @Injectable()
 @RepositoryType(PostType.Photo)
-export class PhotoPostRepository extends BaseMemoryRepository<PhotoPostEntity> {
-  constructor(protected readonly entityFactory: PhotoPostFactory) {
-    super(entityFactory)
+export class PhotoPostRepository extends BasePostgresRepository<PhotoPostEntity> {
+  constructor(protected readonly entityFactory: PhotoPostFactory, readonly client: PrismaClientService) {
+    super(entityFactory, client)
+  }
+
+  public async save(entity: VideoPostEntity): Promise<void> {
+    const pojoEntity = entity.toPOJO()
+    const record = await this.client.post.create({
+      data: {
+        ...pojoEntity,
+        tags: {
+          connect: pojoEntity.tags.map(({ id }) => ({ id }))
+        },
+        comments: { connect: [] },
+        postPhoto: { create: pojoEntity.postPhoto },
+        postQuote: undefined,
+        postText: undefined,
+        postVideo: undefined,
+        postLink: undefined
+      }
+    })
+
+    entity.id = record.id
   }
 }
