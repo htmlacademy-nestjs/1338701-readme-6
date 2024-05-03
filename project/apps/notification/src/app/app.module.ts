2daf057acff02892d3df74c06d0ad2246298e838
IvanMonichev <risen15@yandex.ru>
2024-05-03 19:47:05 +0300

    feat: update env

--- project/apps/notification/src/app/app.module.ts
+++ project/apps/notification/src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
-import { getMongooseOptions } from '@project/config'
 import { EmailSubscriberModule } from '@project/email-subscriber'
-import { NotifyConfigModule } from '@project/notification-config'
+import { getMongooseOptions, NotifyConfigModule } from '@project/notification-config'
 
 @Module({
   imports: [MongooseModule.forRootAsync(getMongooseOptions()), NotifyConfigModule, EmailSubscriberModule],
