efc206037e12207483c3791a48d4fcc43f4250ad
IvanMonichev <monichevid@baltech.ru>
2024-05-07 11:07:55 +0300

    feat: update jwt-refresh strategy

--- project/libs/user/authentication/src/authentication-module/strategies/jwt-refresh.strategy.ts
+++ project/libs/user/authentication/src/authentication-module/strategies/jwt-refresh.strategy.ts
@@ -1,8 +1,10 @@
 import { Inject, Injectable } from '@nestjs/common'
 import { PassportStrategy } from '@nestjs/passport'
 import { jwtConfig } from '@project/config'
-import { ITokenPayload } from '@project/shared/core'
+import { IRefreshTokenPayload } from 'libs/shared/core/src/types/token/refresh-token-payload.interface'
 import { AuthenticationService } from 'libs/user/authentication/src/authentication-module/authentication.service'
+import { TokenNotExistsException } from 'libs/user/authentication/src/authentication-module/exceptions/token-not-exists.exception'
+import { RefreshTokenService } from 'libs/user/authentication/src/refresh-token-module/refresh-token.service'
 import { ExtractJwt, Strategy } from 'passport-jwt'
 import { ConfigType } from '@nestjs/config'
 
@@ -10,7 +12,8 @@ import { ConfigType } from '@nestjs/config'
 export class JwtRefreshStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
   constructor(
     @Inject(jwtConfig.KEY) private readonly jwtOptions: ConfigType<typeof jwtConfig>,
-    private readonly authService: AuthenticationService
+    private readonly authService: AuthenticationService,
+    private readonly refreshTokenService: RefreshTokenService
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -18,7 +21,14 @@ export class JwtRefreshStrategy extends PassportStrategy(Strategy, 'jwt-refresh'
     })
   }
 
-  public async validate(payload: ITokenPayload) {
+  public async validate(payload: IRefreshTokenPayload) {
+    if (!(await this.refreshTokenService.isExists(payload.tokenId))) {
+      throw new TokenNotExistsException(payload.tokenId)
+    }
+
+    await this.refreshTokenService.deleteRefreshSession(payload.tokenId)
+    await this.refreshTokenService.deleteExpiredRefreshTokens()
+
     return this.authService.getUserByEmail(payload.email)
   }
 }
